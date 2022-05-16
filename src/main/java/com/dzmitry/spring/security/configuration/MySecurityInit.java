d53d0bab44fb178a040e519de3bac53fda6076c3
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-05-16 13:42:08 +0200

    init

---
+++ src/main/java/com/dzmitry/spring/security/configuration/MySecurityInit.java
@@ -0,0 +1,6 @@
+package com.dzmitry.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ target/spring_course_security/WEB-INF/view/view-for-managers.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h3>here you can see performance of your employees</h3>
+
+</body>
+</html>
